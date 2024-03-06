55d686ff1f89aeaa98520c211d8ab01c247f533e
Irlan Abushakhmanov <irlanidzzze@gmail.com>
2024-03-06 17:44:19 +0300

    Added source code

---
+++ NextAnimation/NextAnimation/NextAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextAnimationApp.swift
+//  NextAnimation
+//
+//  Created by Irlan Abushakhmanov on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NextAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
